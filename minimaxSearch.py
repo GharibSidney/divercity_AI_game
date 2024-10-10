429a21899c5c06a28e361ddbc67d556379e3baa3
GharibSidney <sidney-gharib@hotmail.com>
2024-10-10 11:43:59 -0400

    fixed minmax

--- minimaxSearch.py
+++ minimaxSearch.py
@@ -1,27 +1,32 @@
 from seahorse.game.action import Action
 from seahorse.game.game_state import GameState
 # éventuellement modifier le prochain import
-from my_player_beats_greedy import  MyPlayer 
+from players.my_player_beats_greedy import  MyPlayer 
 
 
 class minimaxSearch():
 
-    def __init__(self, player:MyPlayer):
+    def __init__(self, player:MyPlayer, state:GameState):
         self.player = player
+        self.opponent_id = self.player.get_opponent_id(state)
+        self.id = self.player.get_id()
+        # max depth of the tree
         self.depth_max = 3
         self.counter = 0
 
-    def minimaxSearch(self, state: GameState, player: MyPlayer,  isMax:bool):
+    def minimaxSearch(self, state: GameState, isMax:bool): #player: MyPlayer,
         self.counter = 0
         if isMax: v, m = self.maxValue(state, self.counter)
         else: v, m = self.minValue(state, self.counter)
         return v, m
 
     def maxValue(self, state: GameState):
-        if self.isTerminal(state):
-            return #state # score # return something see class notes
+        if self.isTerminal():
+            return action.get_next_game_state().scores[self.id] - action.get_next_game_state().scores[self.opponent_id], None
+        
         v_star = -1000000
         m_star = None
+
         for action in self.getPossibleActions(state):
             temporary_state = self.transition(state, action)
             self.counter += 1
@@ -32,8 +37,10 @@ class minimaxSearch():
         return v_star, m_star
 
     def minValue(self, state: GameState):
-        if self.isTerminal(state):
-            return #state # score # return something see class notes
+        if self.isTerminal():
+            # the best score is reversed because we are the opponent
+            return action.get_next_game_state().scores[self.opponent_id] - action.get_next_game_state().scores[self.id], None
+        
         v_star = 1000000
         m_star = None
         for action in self.getPossibleActions(state):
@@ -45,16 +52,15 @@ class minimaxSearch():
                 m_star = action
         return v_star, m_star
         
-    def isTerminal(self, state: GameState):
+    def isTerminal(self):
         return self.depth_max == self.counter
 
     def getPossibleActions(self, state: GameState):
         # J'ai mis un fonction dans une fonction, car 
         # si jamais on veut changer pour light action
-        # on aura qu'à changer le return
+        # on aura qu'à changer le return 
         return state.generate_possible_heavy_actions()
 
-    def transition(self, state: GameState, action: Action):
-        # opponent_id = self.player.get_opponent_id(state)
-        # best_score = action.get_next_game_state().scores[self.player.get_id()] - action.get_next_game_state().scores[opponent_id]
+    def transition(self, action: Action):
+        
         return action.get_next_game_state()
