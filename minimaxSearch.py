be9d4769f7c13e594a5613aecd97a867f8e3df9a
GharibSidney <sidney-gharib@hotmail.com>
2024-10-10 10:55:43 -0400

    finished min max

--- minimaxSearch.py
+++ minimaxSearch.py
@@ -6,21 +6,25 @@ from my_player_beats_greedy import  MyPlayer
 
 class minimaxSearch():
 
-    def __init__(self, player:MyPlayer,):
+    def __init__(self, player:MyPlayer):
         self.player = player
+        self.depth_max = 3
+        self.counter = 0
 
     def minimaxSearch(self, state: GameState, player: MyPlayer,  isMax:bool):
-        if isMax: v, m = self.maxValue(state)
-        else: v, m = self.minValue(state)
+        self.counter = 0
+        if isMax: v, m = self.maxValue(state, self.counter)
+        else: v, m = self.minValue(state, self.counter)
         return v, m
 
     def maxValue(self, state: GameState):
         if self.isTerminal(state):
             return #state # score # return something see class notes
-        v_star = float('-inf')
+        v_star = -1000000
         m_star = None
         for action in self.getPossibleActions(state):
             temporary_state = self.transition(state, action)
+            self.counter += 1
             v, _ = self.minValue(temporary_state)
             if  v > v_star:
                 v_star = v
@@ -30,18 +34,19 @@ class minimaxSearch():
     def minValue(self, state: GameState):
         if self.isTerminal(state):
             return #state # score # return something see class notes
-        v_star = float('inf')
+        v_star = 1000000
         m_star = None
         for action in self.getPossibleActions(state):
             temporary_state = self.transition(state, action)
+            self.counter += 1
             v, _ = self.maxValue(temporary_state)
             if  v < v_star:
                 v_star = v
                 m_star = action
         return v_star, m_star
         
-    def isTerminal(self, state: GameState, depth_max: int):
-        NotImplementedError()
+    def isTerminal(self, state: GameState):
+        return self.depth_max == self.counter
 
     def getPossibleActions(self, state: GameState):
         # J'ai mis un fonction dans une fonction, car 
@@ -50,7 +55,6 @@ class minimaxSearch():
         return state.generate_possible_heavy_actions()
 
     def transition(self, state: GameState, action: Action):
-        opponent_id = self.player.get_opponent_id(state)
-        best_score = action.get_next_game_state().scores[self.player.get_id()] - action.get_next_game_state().scores[opponent_id]
-
-        NotImplementedError()
+        # opponent_id = self.player.get_opponent_id(state)
+        # best_score = action.get_next_game_state().scores[self.player.get_id()] - action.get_next_game_state().scores[opponent_id]
+        return action.get_next_game_state()
